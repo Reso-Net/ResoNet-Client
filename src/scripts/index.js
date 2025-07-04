19495c2c7bf4eab6d295fa2d707b851b1796e5dc
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2025-07-04 19:18:22 +1000

    Almost a complete rewrite

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 const ResoNetLib = require('resonet-lib');
+const crypto = require('crypto');
 const fs = require('fs').promises;
 const path = require('path');
 
@@ -156,10 +157,58 @@ async function createContact(contact) {
 }
 
 async function updateContactStatus(status) {
-    const userItem = document.getElementById(status.userId);
-    var onlineStatus = status.sessionType == "Headless" ? "Headless" : status.onlineStatus;
-    if (onlineStatus == null) return;
-    userItem.setAttribute('status', onlineStatus);
+    try {
+        console.log(status);
+        var onlineStatus = status.sessionType == "Headless" ? "Headless" : status.onlineStatus;
+
+        const userItem = document.getElementById(status.userId);
+        userItem.setAttribute('status', onlineStatus);
+
+        const userInfo = userItem.querySelector('#userInfo');
+
+        var currentSession = status.sessions[status.currentSessionIndex];
+        var isHidden = currentSession.sessionHidden;
+        var isHost = currentSession.isHost
+        var hashSalt = status.hashSalt;
+
+        console.log(hashIDToToken(currentSession.sessionHash, hashSalt));
+        var sessionName;
+        switch (currentSession.accessLevel) {
+            case "Private":
+            case "LAN":
+                sessionName = `a ${currentSession.accessLevel} World`;
+                break;
+
+            case "Contacts":
+                sessionName = `a Contacts Only World`;
+                break;
+
+            case "ContactsPlus":
+                sessionName = `a Contacts Plus World`;
+                break;
+
+            case "RegisteredUsers":
+            case "Anyone":
+                // Actually set session name if can get
+                sessionName = `a Publically accesible world World`;
+                break;
+
+            default: 
+                sessionName = `an Unkown World`;
+        }
+
+        userInfo.textContent = `${userItem.getAttribute('username')}\n${onlineStatus} in ${sessionName}` 
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+function hashIDToToken(id, salt = '') {
+  const hash = crypto.createHash('sha256')
+                     .update(id + salt, 'utf8')
+                     .digest('hex')
+                     .toUpperCase();
+  return hash;
 }
 
 async function selectUser(userId) {
